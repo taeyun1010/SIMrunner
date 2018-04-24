8af0106b274be7c1cf94af4aee82d523662a2d37
Taeyun Kim <taeyun1010@gmail.com>
2018-04-24 20:41:05 +0900

    working?

--- main.py
+++ main.py
@@ -17,11 +17,15 @@ def rSubset(arr, r):
 
 # gets percentage given line, returns int type
 def getPercentage(line):
-    index = thisline.find("consists for")
-    index = index + 13
-    split = thisline[index:]
-    endindex = split.find("%") - 1
-    return int(split[:endindex])
+    #index = thisline.find("for ")
+    #index = index
+    #split = thisline[index:]
+#     endindex = split.find("%")-1
+#     split = split[:endindex]
+    p = [int(s) for s in line.split() if s.isdigit()]
+    #print(split[:endindex])
+    #return int(split[:endindex])
+    return p[0]
 
 # gets first file's extension, or "" if no file extension found 
 def getFirstFileExtension(line):
@@ -104,29 +108,51 @@ if __name__ == '__main__':
                 for thisline in fi:
                     # write only if the line contains both coins being compared
                     if ((coin1 in thisline) and (coin2 in thisline)):
-                        count = count + 1
-                        p = getPercentage(thisline)
-                        totalsum = totalsum + p
+                        #count = count + 1
+                        #p = getPercentage(thisline)
+                        #totalsum = totalsum + p
                         fo.write(thisline)
-            f = open("processedresult.txt", "r")
-            lines = f.readlines()
-            f.close
-            firstline = lines[0]
-            
-            requirementResult = meetsRequirement(firstline)
-            
-            # if has wanted extension
-            if (requirementResult == 1):
-                print("meets requirement")
-            # else, delete all the lines containing the file with not wanted extension
-            else:
-                with open("processedresult.txt", "w+") as fo:
-                    if (requirementResult == 0):
-                        filetodelete = getFirstFile(firstline)
-                    else:
-                        filetodelete = getSecondFile(firstline)
+                        
+            while(1):
+                f = open("processedresult.txt", "r")
+                lines = f.readlines()
+                f.close
+                if (len(lines) == 0):
+                    break
+                firstline = lines[0]
+                
+                requirementResult = meetsRequirement(firstline)
+                filetosearch = getFirstFile(firstline)
+                
+                # if has wanted extension
+                if (requirementResult == 1):
+                    count = count + 1
+                    maxpercentage = 0
                     for line in lines:
-                        if filetodelete not in line:
-                            fo.write(line)
+                        if (filetosearch == getFirstFile(line)):
+                            if (meetsRequirement(line) == 1):
+                                thispercentage = getPercentage(line)
+                                if (thispercentage > maxpercentage):
+                                    maxpercentage = thispercentage
+                    print(filetosearch + " showed max of " + str(maxpercentage) + " similarity \n")
+                    totalsum = totalsum + maxpercentage
+                    # done with this file, delete all lines containing this file as first file
+                    with open("processedresult.txt", "w+") as fo:
+                        for line in lines:
+                            if (getFirstFile(line) != filetosearch):
+                                fo.write(line)
+                    
+                # else, delete all the lines containing the file with not wanted extension
+                else:
+                    with open("processedresult.txt", "w+") as fo:
+                        if (requirementResult == 0):
+                            filetodelete = getFirstFile(firstline)
+                        else:
+                            filetodelete = getSecondFile(firstline)
+                        for line in lines:
+                            if filetodelete not in line:
+                                fo.write(line)
             
             print("Similarity percentage between " + thiscombi[0] + " and " + thiscombi[1] + " = " + str((totalsum / count)))
+            
+        
